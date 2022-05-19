36752e8d7aa095652cfd382048b6d9b45f62f3d1
nicholasarinduque <89489431+nicholasarinduque@users.noreply.github.com>
2022-05-19 15:42:58 -0700

    4.3.5 done

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,45 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Assets
+public class PlayerController : MonoBehaviour
 {
-    public class PlayerController : MonoBehaviour
+    private Rigidbody playerRb;
+    private GameObject focalPoint;
+    private float powerupStrength = 15.0f;
+    public float speed = 5.0f;
+    public bool hasPowerup = false;
+
+    // Start is called before the first frame update
+    void Start()
     {
-        private Rigidbody playerRb;
-        private GameObject focalPoint;
-        public float speed = 5.0f;
-        public bool hasPowerup = false;
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
+    }
 
-        // Start is called before the first frame update
-        void Start()
-        {
-            playerRb = GetComponent<Rigidbody>();
-            focalPoint = GameObject.Find("Focal Point");
-        }
-
-        // Update is called once per frame
+    // Update is called once per frame
         void Update()
         {
             float forwardInput = Input.GetAxis("Vertical");
 
             playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed);
         }
-
-        private void OnTriggerEnter(Collider other)
+    
+    private void OnTriggerEnter(Collider other)
+    {	
+		if(other.CompareTag("powerup"))
 		{
-			if(other.CompareTag("powerup"))
-			{
 				hasPowerup = true;
 				Destroy(other.gameObject);
-			}
 		}
-
     }
 
-	private void OnCollisionEnter(Collision collision)
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+    }
+    private void OnCollisionEnter(Collision collision)
 	{
 		if(collision.gameObject.CompareTag("Enemy") && hasPowerup)
 		{
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = collision.gameObject.transform.position - transform.postion;
+
+            enemyRigidbody.AddForce(awayFromPlayer * powerUpStrength, ForceMode.Inpules);
 		    Debug.Log("Collided with: " + collison.gameObject.name + " with powerup set to " + hasPowerup);
 		}
 	}
